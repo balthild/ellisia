b2cb65c776ad271c16491efd2743a3d02ebe9cef
Balthild <ibalthild@gmail.com>
2023-11-10 16:48:20 -0600

    Redesign the library

--- src/Library.tsx
+++ src/Library.tsx
@@ -1,7 +1,8 @@
 import './Library.scss';
 
 import { dialog, invoke } from '@tauri-apps/api';
-import { createResource, createSignal, For, onCleanup } from 'solid-js';
+import { createEffect, createResource, createSignal, For, onCleanup } from 'solid-js';
+import { createStore, reconcile } from 'solid-js/store';
 
 import { Toolbar } from './components/Toolbar';
 import { ToolbarIcon } from './components/ToolbarIcon';
@@ -16,39 +17,20 @@ export function Library() {
         window.removeEventListener('focus', refetch);
     });
 
-    const booksSorted = () => {
-        let data = library.latest;
-        if (!data) {
-            return [];
-        }
+    const [books, setBooks] = createStore(transformBooks());
+    createEffect(() => setBooks(reconcile(transformBooks(library.latest))));
 
-        const books = Object.entries(data.books).map(([id, book]) => {
-            let last_read_at = undefined;
-            if (book.last_read_at) {
-                last_read_at = new Date(book.last_read_at);
-            }
-            return {
-                ...book,
-                id,
-                last_read_at,
-            };
-        });
-
-        books.sort((a, b) => {
-            // NaN always goes to the end
-            const timeA = b.last_read_at?.getTime() ?? NaN;
-            const timeB = a.last_read_at?.getTime() ?? NaN;
-            return timeA - timeB;
-        });
-
-        return books;
-    };
-
-    const [selectedBook, setSelectedBook] = createSignal<string>();
+    const [selectedBookId, setSelectedBookId] = createSignal<string>();
 
     const [erroredCover, setErroredCover] = createSignal<string[]>([]);
 
-    const addBook = async () => {
+    const deselectBook = (event: MouseEvent) => {
+        if (event.target === event.currentTarget) {
+            setSelectedBookId(undefined);
+        }
+    };
+
+    const openNewBook = async () => {
         const path = await dialog.open({
             filters: [{ name: 'Epub', extensions: ['epub'] }],
         });
@@ -56,15 +38,14 @@ export function Library() {
             return;
         }
 
-        console.log(path);
         const result = await invoke<boolean>('open_book', { path });
         if (result) {
             refetch();
         }
     };
 
-    const openBook = async (book: EllisiaLibraryBook) => {
-        const result = await invoke<boolean>('open_book', { path: book.path });
+    const openBook = async (path: string) => {
+        const result = await invoke<boolean>('open_book', { path });
         if (result) {
             await invoke<boolean>('close_library');
         }
@@ -73,58 +54,72 @@ export function Library() {
     return (
         <div id="library">
             <Toolbar size="large">
-                <ToolbarIcon
-                    bordered
-                    onClick={addBook}
-                    icon="folder-open-line"
-                    label="Open"
-                />
+                <ToolbarIcon bordered onClick={openNewBook} icon="folder-open-line" label="Open" />
             </Toolbar>
 
-            <table class="books">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Title</th>
-                        <th>Last Read</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <For each={booksSorted()}>
-                        {(book) => (
-                            <tr
-                                onDblClick={[openBook, book]}
-                                onClick={[setSelectedBook, book.id]}
-                                classList={{ selected: selectedBook() === book.id }}
-                            >
-                                <td>
-                                    <img
-                                        class="cover cover-thumbnail"
-                                        classList={{ errored: erroredCover().includes(book.id) }}
-                                        src={`${ELLISIA.renderer}/cover/${book.id}.png`}
-                                        onError={() => setErroredCover((xs) => [...xs, book.id])}
-                                    />
-                                    <div
-                                        class="cover cover-text"
-                                        classList={{ errored: erroredCover().includes(book.id) }}
-                                    >
-                                        {(book.metadata.title ?? getFilename(book.path))[0]}
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="title">
-                                        {book.metadata.title ?? getFilename(book.path)}
-                                    </div>
-                                    <div class="author">{book.metadata.author}</div>
-                                </td>
-                                <td class="nowrap">{book.last_read_at?.toLocaleString()}</td>
-                            </tr>
-                        )}
-                    </For>
-                </tbody>
-            </table>
+            <div class="books" onClick={deselectBook}>
+                <For each={books}>
+                    {(book) => (
+                        <div
+                            class="book"
+                            classList={{ selected: selectedBookId() === book.id }}
+                            onDblClick={[openBook, book.path]}
+                            onClick={[setSelectedBookId, book.id]}
+                        >
+                            {erroredCover().includes(book.id) ? (
+                                <div class="cover cover-text">{book.metadata.title?.[0]}</div>
+                            ) : (
+                                <img
+                                    class="cover cover-thumbnail"
+                                    src={`${ELLISIA.renderer}/cover/${book.id}.png`}
+                                    onError={() => setErroredCover((xs) => [...xs, book.id])}
+                                />
+                            )}
+
+                            <div>
+                                <div class="title">
+                                    {book.metadata.title ?? getFilename(book.path)}
+                                </div>
+                                <div class="author">{book.metadata.author}</div>
+                                <div class="last-read">
+                                    Last Read:
+                                    <br />
+                                    {book.last_read_at?.toLocaleString()}
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </For>
+            </div>
         </div>
     );
+}
+
+function transformBooks(data?: EllisiaLibrary) {
+    if (!data) {
+        return [];
+    }
+
+    const books = Object.entries(data.books).map(([id, book]) => {
+        let last_read_at = undefined;
+        if (book.last_read_at) {
+            last_read_at = new Date(book.last_read_at);
+        }
+        return {
+            ...book,
+            id,
+            last_read_at,
+        };
+    });
+
+    books.sort((a, b) => {
+        // NaN always goes to the end
+        const timeA = b.last_read_at?.getTime() ?? NaN;
+        const timeB = a.last_read_at?.getTime() ?? NaN;
+        return timeA - timeB;
+    });
+
+    return books;
 }
 
 function getFilename(path: string) {
